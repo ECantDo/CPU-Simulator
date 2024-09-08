c6d88f36f8ff5baacc3a8b1d6f647b3a7f78f68c
ECanDo <ecandoecantdo@gmail.com>
2024-09-08 15:02:27 -0600

    Implemented IO in/out

--- src/CPU/IO.java
+++ src/CPU/IO.java
@@ -1,14 +1,27 @@
 package CPU;
 
-import CPU.Peripherals.Screen;
+import CPU.Peripherals.*;
 
 public class IO {
 
-    private final Object IOPorts[];// = new Object[16];
+    private final IOInterface[] IOPorts;// = new Object[16];
 
     public IO() {
-        this.IOPorts = new Object[16];
+        this.IOPorts = new IOInterface[16];
 
         this.IOPorts[0] = new Screen();
     }
+
+    public void ioOutput(int data, int port) {
+        port = port & 0xF; // 4 bit for port
+        data = data & 0xFFFF; // 16 bit being sent
+
+        this.IOPorts[port].ioInput(data);
+    }
+
+    public int ioInput(int port) {
+        port = port & 0xF; // 4 bit for port
+        return (byte)this.IOPorts[port].ioOutput();
+    }
+
 }
