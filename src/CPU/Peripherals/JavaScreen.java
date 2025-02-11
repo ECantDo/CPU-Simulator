598794996b457d9158f6846b9c47200ddaed7db9
ECanDo <ecandoecantdo@gmail.com>
2025-02-10 23:10:48 -0600

    Started working on a java screen

---
+++ src/CPU/Peripherals/JavaScreen.java
@@ -0,0 +1,30 @@
+package CPU.Peripherals;
+
+import CPU.IO;
+
+public class JavaScreen implements ScreenInterface, IOInterface {
+	@Override
+	public void putPixel(int x, int y, boolean color) {
+
+	}
+
+	@Override
+	public void clear() {
+
+	}
+
+	@Override
+	public void update() {
+
+	}
+
+	@Override
+	public void ioInput(int data) {
+
+	}
+
+	@Override
+	public int ioOutput() {
+		return 0;
+	}
+}
