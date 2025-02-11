0ecbe0e8f8b9d0e86fa073fa64eebc471a39ae9c
ECanDo <ecandoecantdo@gmail.com>
2025-02-10 23:17:41 -0600

    Setup for the screen

--- src/CPU/Peripherals/Screen/JavaScreen.java
+++ src/CPU/Peripherals/Screen/JavaScreen.java
@@ -2,7 +2,25 @@ package CPU.Peripherals.Screen;
 
 import CPU.Peripherals.IOInterface;
 
-public class JavaScreen implements ScreenInterface, IOInterface {
+import javax.swing.*;
+import java.awt.*;
+
+public class JavaScreen extends JFrame implements ScreenInterface, IOInterface {
+
+	private final int rows;
+	private final int columns;
+	private final int cellSize;
+
+	private Color[][] gridColors;
+
+	public JavaScreen(int cellSquareWidth, int gridSquareWidth) {
+		this.cellSize = cellSquareWidth;
+		this.columns = gridSquareWidth;
+		this.rows = gridSquareWidth;
+
+		this.gridColors = new Color[this.rows][this.columns];
+	}
+
 	@Override
 	public void putPixel(int x, int y, boolean color) {
 
