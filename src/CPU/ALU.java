7abd9915dc2dd585ffed55ba5d488e048cad24aa
ECanDo <ecandoecantdo@gmail.com>
2025-03-25 00:02:36 -0600

    New opcodes implementation (alu).
    Added the new operations to the ALU functionality.

--- src/CPU/ALU.java
+++ src/CPU/ALU.java
@@ -3,63 +3,50 @@ package CPU;
 public class ALU {
 
 	//==================================================================================================================
-	// Operation codes, both mnemonics and numbers; get index from these
+	// Flags
 	//==================================================================================================================
-	public static final String[] opCodesMnemonics = {
-			"add", "sub", "and", "or", "xor",
-			"nand", "nor", "xnor", "bsh"
-	};
-
-	public static final int[] opCodes = {
-			2, 3, 4, 5, 6,
-			7, 8, 9, 10
-	};
-
-	public static int getOpcode(int opcode) {
-		for (int i = 0; i < opCodes.length; i++) {
-			if (opCodes[i] == opcode) {
-				return i;
-			}
-		}
-		return -1;
-	}
-
-	public static int getOpcode(String mnemonic) {
-		for (int i = 0; i < opCodesMnemonics.length; i++) {
-			if (opCodesMnemonics[i].equals(mnemonic)) {
-				return opCodes[i];
-			}
-		}
-		return -1;
-	}
+	boolean equalsZero;
+	boolean carryOut;
 
 	//==================================================================================================================
 	// Run the CPU.ALU, get the operation and run the proper operation
 	//==================================================================================================================
-	public int run(int opCode, int input_a, int input_b) {
-		return runOperation(getOpcode(opCode), input_a, input_b);
-	}
 
-	public int run(String mnemonic, int input_a, int input_b) {
-		return runOperation(getOpcode(mnemonic), input_a, input_b);
-	}
 
-	private byte runOperation(int operation, int input_a, int input_b) {
+	private int runOperation(int operation, int input_a, int input_b) {
+
+		// The opcode is in the left 4-most bits, if it is 1, 2, or 3, it is an ALU operation.
+		int opcode = operation & 0b00001111;
+		if (!(opcode == 1 || opcode == 2 || opcode == 3)) {
+			return 0;
+		}
+
+		// The sub opcode tells the ALU what operation to perform
+		int subOpcode = (operation & 11100000) >> 5;
+
 		// Refer to the opCodes array for the operation value
 		// Doing it this way makes it really easy to add new operations
-		return switch (operation) {
-			// TODO: Fix the ALU output table
-//            case 0 -> add(input_a, input_b);
-//            case 1 -> sub(input_a, input_b);
-//            case 2 -> and(input_a, input_b);
-//            case 3 -> or(input_a, input_b);
-//            case 4 -> xor(input_a, input_b);
-//            case 5 -> nand(input_a, input_b);
-//            case 6 -> nor(input_a, input_b);
-//            case 7 -> xnor(input_a, input_b);
-//            case 8 -> bsh(input_a, input_b);
-			default -> 0;
-		};
+		if (opcode == 1)
+			return switch (subOpcode) {
+				case 0 -> add(input_a, input_b);
+				case 1 -> sub(input_a, input_b);
+				case 2 -> xor(input_a, input_b);
+				case 3 -> or(input_a, input_b);
+				case 4 -> and(input_a, input_b);
+				case 5 -> xnor(input_a, input_b);
+				case 6 -> nor(input_a, input_b);
+				case 7 -> nand(input_a, input_b);
+				default -> 0;
+			};
+		else if (opcode == 2)
+			return switch (subOpcode) {
+				case 0 -> lshift(input_a, input_b);
+				case 2 -> arshift(input_a, input_b);
+				case 3 -> rshift(input_a, input_b);
+				default -> 0;
+			};
+
+		return addWithFlags(input_a, input_b);
 	}
 
 	//==================================================================================================================
@@ -67,6 +54,10 @@ public class ALU {
 	//==================================================================================================================
 	private int add(int input_a, int input_b) {
 		return (input_a + input_b) & CPUSpecs.bitMask;
+	}
+
+	private  int addWithFlags(int input_a, int input_b){
+		return (input_a + input_b + (carryOut? 1 : 0)) & CPUSpecs.bitMask;
 	}
 
 	private int sub(int input_a, int input_b) {
@@ -104,6 +95,12 @@ public class ALU {
 
 	private int rshift(int input_a, int input_b) {
 		input_b = input_b & 0xF; // 16-bit, only able to shift 15 times
+		return (input_a >>> input_b) & CPUSpecs.bitMask;
+	}
+
+	private int arshift(int input_a, int input_b) {
+		input_b = input_b & 0xF; // 16-bit, only able to shift 15 times
+		input_a |= -1 << CPUSpecs.bitCount; // Make everything above bit 16 a 1, so when right shifting 1s will fill
 		return (input_a >> input_b) & CPUSpecs.bitMask;
 	}
 
