7b92cb3f70fd825e763c64c403dbce835cee2cf5
ECanDo <ecandoecantdo@gmail.com>
2025-03-26 21:39:33 -0600

    Updated ProgramCounter.
    You can still get the value in the program counter.  There is now an increment function.  You can still set the value.
    Updated to work with CPUSpecs.

--- src/CPU/ProgramCounter.java
+++ src/CPU/ProgramCounter.java
@@ -5,18 +5,17 @@ public class ProgramCounter {
     // TODO: Program counter is changing... class needs reworking.
     private int PROGRAM_COUNTER = 0;
 
-    public void set(int value) {
-        PROGRAM_COUNTER = value;
-        PROGRAM_COUNTER = PROGRAM_COUNTER & CPUSpecs.romAddressSpaceMask;
+    public void setProgramCounter(int value){
+        this.PROGRAM_COUNTER = value & CPUSpecs.romAddressSpaceMask;
     }
 
-    public void increment() {
-        PROGRAM_COUNTER++;
-        PROGRAM_COUNTER = PROGRAM_COUNTER & CPUSpecs.romAddressSpaceMask;
+    public void increment(int amount) {
+        this.PROGRAM_COUNTER += amount;
+        this.PROGRAM_COUNTER = this.PROGRAM_COUNTER & CPUSpecs.romAddressSpaceMask;
     }
 
     public int getProgramCounter() {
-        return PROGRAM_COUNTER;
+        return this.PROGRAM_COUNTER;
     }
 
 }
