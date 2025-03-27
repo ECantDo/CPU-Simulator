537244948d464d2eedeab8d49e113d5f274d0d71
ECanDo <ecandoecantdo@gmail.com>
2025-03-26 21:32:27 -0600

    Implemented current flags to the ALU.

--- src/CPU/ALU.java
+++ src/CPU/ALU.java
@@ -5,14 +5,22 @@ public class ALU {
 	//==================================================================================================================
 	// Flags
 	//==================================================================================================================
-	boolean equalsZero;
-	boolean carryOut;
+	public boolean equalsZero;
+	public boolean carryOut;
+
+
+	public ALU() {
+		this(false, false);
+	}
+
+	public ALU(boolean equalsZero, boolean carryOut) {
+		this.equalsZero = equalsZero;
+		this.carryOut = carryOut;
+	}
 
 	//==================================================================================================================
 	// Run the CPU.ALU, get the operation and run the proper operation
 	//==================================================================================================================
-
-	//TODO; implement flags
 	private int runOperation(int operation, int input_a, int input_b) {
 
 		// The opcode is in the left 4-most bits, if it is 1, 2, or 3, it is an ALU operation.
@@ -23,6 +31,9 @@ public class ALU {
 
 		// The sub opcode tells the ALU what operation to perform
 		int subOpcode = (operation & 11100000) >> 5;
+
+		input_a &= CPUSpecs.bitMask;
+		input_b &= CPUSpecs.bitMask;
 
 		// Refer to the opCodes array for the operation value
 		// Doing it this way makes it really easy to add new operations
@@ -53,39 +64,66 @@ public class ALU {
 	// Operations
 	//==================================================================================================================
 	private int add(int input_a, int input_b) {
-		return (input_a + input_b) & CPUSpecs.bitMask;
+		int res = input_a + input_b;
+		this.carryOut = (res & 0x10000) != 0;
+		this.equalsZero = res == 0;
+		return res & CPUSpecs.bitMask;
 	}
 
-	private  int addWithFlags(int input_a, int input_b){
-		return (input_a + input_b + (carryOut? 1 : 0)) & CPUSpecs.bitMask;
+	private int addWithFlags(int input_a, int input_b) {
+		int res = input_a + input_b + (carryOut ? 1 : 0);
+		this.carryOut = (res & 0x10000) != 0;
+		this.equalsZero = res == 0;
+		return res & CPUSpecs.bitMask;
 	}
 
 	private int sub(int input_a, int input_b) {
-		return (input_a - input_b) & CPUSpecs.bitMask;
+		int res = input_a - input_b;
+		this.carryOut = (res & 0x10000) != 0;
+		this.equalsZero = res == 0;
+		return res & CPUSpecs.bitMask;
 	}
 
 	private int and(int input_a, int input_b) {
-		return (input_a & input_b) & CPUSpecs.bitMask;
+		int res = input_a & input_b;
+		this.carryOut = false;
+		this.equalsZero = res == 0;
+		return res & CPUSpecs.bitMask;
 	}
 
 	private int or(int input_a, int input_b) {
-		return (input_a | input_b) & CPUSpecs.bitMask;
+		int res = input_a | input_b;
+		this.carryOut = false;
+		this.equalsZero = res == 0;
+		return res & CPUSpecs.bitMask;
 	}
 
 	private int xor(int input_a, int input_b) {
-		return (input_a ^ input_b) & CPUSpecs.bitMask;
+		int res = input_a ^ input_b;
+		this.carryOut = false;
+		this.equalsZero = res == 0;
+		return res & CPUSpecs.bitMask;
 	}
 
 	private int nand(int input_a, int input_b) {
-		return ~(input_a & input_b) & CPUSpecs.bitMask;
+		int res = ~(input_a & input_b);
+		this.carryOut = false;
+		this.equalsZero = res == 0;
+		return res & CPUSpecs.bitMask;
 	}
 
 	private int nor(int input_a, int input_b) {
-		return ~(input_a | input_b) & CPUSpecs.bitMask;
+		int res = ~(input_a | input_b);
+		this.carryOut = false;
+		this.equalsZero = res == 0;
+		return res & CPUSpecs.bitMask;
 	}
 
 	private int xnor(int input_a, int input_b) {
-		return ~(input_a ^ input_b) & CPUSpecs.bitMask;
+		int res = ~(input_a ^ input_b);
+		this.carryOut = false;
+		this.equalsZero = res == 0;
+		return res & CPUSpecs.bitMask;
 	}
 
 	private int lshift(int input_a, int input_b) {
@@ -100,7 +138,8 @@ public class ALU {
 
 	private int arshift(int input_a, int input_b) {
 		input_b = input_b & 0xF; // 16-bit, only able to shift 15 times
-		input_a |= -1 << CPUSpecs.bitCount; // Make everything above bit 16 a 1, so when right shifting 1s will fill
+		if ((input_a & 8000) != 0)  // Only populate upper bits when the sign bit is 1
+			input_a |= -1 << CPUSpecs.bitCount; // Make everything above bit 16 a 1, so when right shifting 1s will fill
 		return (input_a >> input_b) & CPUSpecs.bitMask;
 	}
 
