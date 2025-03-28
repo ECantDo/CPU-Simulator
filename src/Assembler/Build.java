12999ab8636d38417ed6a386cf5a7b9e9981cc98
ECanDo <ecandoecantdo@gmail.com>
2025-03-28 00:04:16 -0600

    Implemented methods to start filtering out un-needed parts of the inputted code

--- src/Assembler/Build.java
+++ src/Assembler/Build.java
@@ -1,12 +1,23 @@
 package Assembler;
 
+import Assembler.Exceptions.OpcodeExistsException;
+import Assembler.Operations.Opcodes;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Build {
+
+	/**
+	 * @param programPath
+	 * @return
+	 */
 	public static int[] build(String programPath) {
+//		TODO:
+//		 Make the stack pointer point to the proper position in the stack
 		ArrayList<String> programLinesList = new ArrayList<>();
 
 		// Get file contents
@@ -17,30 +28,76 @@ public class Build {
 			throw new RuntimeException(e);
 		}
 		while (scanner.hasNextLine()) {
-			String line = scanner.nextLine();
+			String line = scanner.nextLine().toLowerCase();
 			programLinesList.add(line);
 		}
 
-		String[] filteredProgramLines = filterProgram(programLinesList);
-		int[] programValues = new int[filteredProgramLines.length];
+		programLinesList = filterProgramComments(programLinesList);
+
+		int[] programValues = new int[programLinesList.size()];
 
 		return programValues;
 	}
 
-	private static String[] filterProgram(ArrayList<String> programLines) {
+	/**
+	 * Remove all comments and blanklines from a given program.
+	 *
+	 * @param programLines {@link ArrayList} of lines in the program.
+	 * @return Returns a new ArrayList with the new program.
+	 */
+	private static ArrayList<String> filterProgramComments(ArrayList<String> programLines) {
 		if (programLines == null)
 			throw new IllegalArgumentException("programLines cannot be null");
 
 		ArrayList<String> outputLines = new ArrayList<>(programLines.size());
 
 		for (String line : programLines) {
-			line = line.strip();
-			if (line.startsWith("//"))
+			int stripIdx = line.indexOf("//") - 1;
+			if (stripIdx >= 0)
+				line = line.substring(0, stripIdx);
+
+			line = line.strip().replaceAll("\t", " ");
+
+			if (!line.isEmpty())
+				outputLines.add(line);
+		}
+		return outputLines;
+	}
+
+
+	private static Map<String, Integer> filterConstants(ArrayList<String> programLines){
+		for (int i = 0; i < programLines.size(); i++){
+			String line = programLines.get(i);
+			String[] elements = line.split(" ");
+			if (!elements[0].equals("def"))
 				continue;
 
-			outputLines.add(line); // todo: finish
+			if (Opcodes.operationExists(elements[1]))
+				throw new OpcodeExistsException();
+
 		}
-		return outputLines.toArray(new String[]{});
+	}
+
+	private static int parseValue(String value){
+		// Parse char
+		if (value.charAt(0) == '\'' && value.charAt(value.length() - 1) == '\'' ){
+				if (value.length() <= 2 || value.length() > 4)
+					throw new IllegalArgumentException("Character parsing requires characters to be a single " +
+							"character. Either a single letter, or an escape character.");
+
+			value = value.substring(1, value.length() - 1);
+
+			if (value.charAt(0) == '\\'){
+				return switch (value.charAt(1)){
+					case 'r' -> '\r';
+					case 'n' -> '\n';
+					default -> throw new IllegalStateException("Unexpected value: " + value.charAt(1));
+				};
+			}
+			return value.charAt(0);
+		}
+
+		// todo: parse int, octal, bin, hex
 	}
 
 }
