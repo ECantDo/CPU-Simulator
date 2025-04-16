f5178957f24e24a4bd63806188ce3fea6ea9c1ae
ECanDo <ecandoecantdo@gmail.com>
2025-04-15 19:07:47 -0600

    Testing build main program

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,12 @@ import CPU.CPUExecutionCycle;
 public class Main {
 	public static void main(String[] args) {
 
-		String programPath = "src\\Programs_V1_0\\bouncingBall.as";
+		String programPath = "src\\Programs_V2_0\\fib.as";
 
 		// TODO; double check that it works as intended when finished with the build program
 		int speed = 10;
-//		int[] program = Assembler.Build.build(programPath);
-		int[] program = {0};
-		CPUExecutionCycle CPUExecutionCycle = new CPUExecutionCycle(program, speed);
-		CPUExecutionCycle.loop();
+		int[] program = Assembler.Build.build(programPath);
+//		CPUExecutionCycle cpuExecutionCycle = new CPUExecutionCycle(program, speed);
+//		CPUExecutionCycle.loop();
 	}
 }
