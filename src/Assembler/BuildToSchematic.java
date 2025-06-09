5b73540f6ab0b40fbcb6f8fd005f24c9736b966b
ECanDo <ecandoecantdo@gmail.com>
2025-06-09 17:39:54 -0600

    Writes the compiled program to an ascii binary file.

--- src/Assembler/BuildToSchematic.java
+++ src/Assembler/BuildToSchematic.java
@@ -1,10 +1,27 @@
 package Assembler;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class BuildToSchematic {
 
 	public static void main(String[] args) {
 		String file_name = "src/Programs_V2_0/fib";
-		Build.build(file_name + ".as");
+		int[] program = Build.build(file_name + ".as");
+		writeToAsciiBinary(file_name, program);
 //		Build.runBuildToSchem(file_name + ".bin");
+	}
+
+	private static void writeToAsciiBinary(String file, int[] program) {
+		file += ".bin";
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+			for (int i : program) {
+				writer.write(String.format("%32s", Integer.toBinaryString(i)).replace(' ', '0'));
+				writer.newLine();
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
