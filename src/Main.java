9e3edeeb96ccfd8f3438de9a151dcc6667302679
ECanDo <eric.opseth@gmail.com>
2026-08-04 18:00:54 -0600

    Fixed an issue with the pathing...
    Now should be compatable on all OS'

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 import CPU.ExecutionLoop;
 
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
 
-        String programPath = "src\\testing.as";
+        String programPath = "." + File.separator + "src" + File.separator + "fib.as";
+        System.out.println(programPath);
 
         int speed = 10;
         int[] program = Assembler.Build.build(programPath);
